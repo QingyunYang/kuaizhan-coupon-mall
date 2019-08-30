3eb4b045c209081163f7b8b9358ac3495ff6ef66
yangqingyun <yangqingyun@hotmail.com>
2019-08-30 13:23:01 +0800

    Updates

---
+++ precache-manifest.6ca4d129f83b0dbe2d832cff5da9ae1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37161ff20cf644dbf08ae5d3572db221",
+    "url": "/kuaizhan-coupon-mall/index.html"
+  },
+  {
+    "revision": "618c7d5b21612133372b",
+    "url": "/kuaizhan-coupon-mall/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "972eb649a57fad8666c5",
+    "url": "/kuaizhan-coupon-mall/static/js/2.f718439e.chunk.js"
+  },
+  {
+    "revision": "618c7d5b21612133372b",
+    "url": "/kuaizhan-coupon-mall/static/js/main.2726e807.chunk.js"
+  },
+  {
+    "revision": "40a8a41620ab4f37077a",
+    "url": "/kuaizhan-coupon-mall/static/js/runtime~main.688afd1d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kuaizhan-coupon-mall/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/kuaizhan-coupon-mall/precache-manifest.6ca4d129f83b0dbe2d832cff5da9ae1a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kuaizhan-coupon-mall/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
